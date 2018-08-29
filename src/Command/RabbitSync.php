9a526baf859c54e885a063497aa6b7fb864eefe7
Vydrin Alexander <a.vydrin@rambler-co.ru>
2018-08-29 15:05:31 +0300

    feature/more-5658

---
+++ src/Command/RabbitSync.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Adtechpotok\Bundle\EnqueueMessengerAdapterBundle\Command;
+
+use Adtechpotok\Bundle\EnqueueMessengerAdapterBundle\Transport\AmqpContextManager;
+use Enqueue\MessengerAdapter\ContextManager;
+use Enqueue\MessengerAdapter\QueueInteropTransport;
+use Interop\Amqp\AmqpContext;
+use Interop\Queue\PsrContext;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Exception\InvalidArgumentException;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class RabbitSync extends Command
+{
+    /** @var string */
+    protected static $defaultName = 'rabbit:sync';
+
+    /** @var ContextManager */
+    private $contextManager;
+    /** @var array[] */
+    private $destinations;
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param iterable|QueueInteropTransport[] $transports
+     * @param PsrContext                       $psrContext
+     *
+     * @throws InvalidArgumentException
+     */
+    public function __construct(iterable $transports, PsrContext $psrContext)
+    {
+        if (! $psrContext instanceof AmqpContext) {
+            throw new InvalidArgumentException(
+                sprintf(
+                    '$psrContext should be an instance of "%s" interface',
+                    AmqpContext::class
+                )
+            );
+        }
+
+        $this->contextManager = new AmqpContextManager($psrContext);
+
+        foreach ($transports as $transport) {
+            $this->destinations[] = $transport->getDestination(null);
+        }
+
+        parent::__construct();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): void
+    {
+        foreach ($this->destinations as $destination) {
+            $this->contextManager->ensureExists($destination);
+        }
+    }
+}
--- src/DependencyInjection/EnqueueMessengerAdapterExtension.php
+++ src/DependencyInjection/EnqueueMessengerAdapterExtension.php
@@ -3,14 +3,19 @@
 namespace Adtechpotok\Bundle\EnqueueMessengerAdapterBundle\DependencyInjection;
 
 use Adtechpotok\Bundle\EnqueueMessengerAdapterBundle\Transport\QueueInteropTransportFactory;
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class EnqueueMessengerAdapterExtension extends Extension implements CompilerPassInterface
 {
     public function load(array $configs, ContainerBuilder $container)
     {
+        $fileLocator = new FileLocator(__DIR__.'/../Resources/config');
+        $loader = new YamlFileLoader($container, $fileLocator);
+        $loader->load('services.yml');
     }
 
     public function process(ContainerBuilder $container)
